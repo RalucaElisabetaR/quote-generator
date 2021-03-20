a7e0374424969463661ac152d3d3178736073ff7
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-20 18:19:03 +0100

    Popolate UI with dynamic quote text and quote author.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-generator');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 // new quote function
@@ -5,7 +12,20 @@ let apiQuotes = [];
 function newQuote() {
   // pick random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // check if author field is blank and replace with unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // quote lenght check to determine styling
+
+  if (quote.text.lenght > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
 
 
