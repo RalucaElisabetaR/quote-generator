0a730af1c5b2ef1d72b852acbff0a63bab7b89f6
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-21 12:15:15 +0100

    create loader with Javascript

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
-const quoteContainer = document.getElementById('quote-generator');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-
-let apiQuotes = [];
-
-// new quote function
-
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function removeLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+// Show New Quote
 function newQuote() {
-  // pick random quote from apiQuotes array
+  showLoadingSpinner();
+  // Pick a random quote from array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // check if author field is blank and replace with unknown
+  // Check if Author field is blank and replace it with 'Unknown'
   if (!quote.author) {
-    authorText.textContent = "Unknown";
+    authorText.textContent = 'Unknown';
   } else {
     authorText.textContent = quote.author;
   }
-  // quote lenght check to determine styling
-
-  if (quote.text.lenght > 50) {
+  // reduce fontsize for long quotes
+  if (quote.text.length > 120) {
     quoteText.classList.add('long-quote');
   } else {
     quoteText.classList.remove('long-quote');
   }
   quoteText.textContent = quote.text;
+  removeLoadingSpinner();
 }
 
-
-// get Quotes from API
+// Get Quotes From API
 async function getQuotes() {
-  const apiUrl = 'https:type.fit/api/quotes';
+  showLoadingSpinner();
+  const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    newQuote()
+    newQuote();
   } catch (error) {
-    // catch error
+    // Catch Error Here
   }
 }
 
-// tweet quote
-
+// Tweet Quote
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
   window.open(twitterUrl, '_blank');
 }
-// event listners
+
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-
-// on load
+// On Load
 getQuotes();
